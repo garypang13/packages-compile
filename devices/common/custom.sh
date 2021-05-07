cba5ba8367b7e6fe642765a47e8968f71a912d0c
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-05-08 06:06:08 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -5,7 +5,7 @@ shopt -s extglob
 sed -i '/	refresh_config();/d' scripts/feeds
 
 rm -rf feeds/custom/{frp,xray-core,oaf,luci-app-{turboacc,mtwifi},naiveproxy,aria2,MentoHUST*,bpytop}
-rm -rf feeds/packages/libs
+rm -rf feeds/packages/libs/!(freetype,libev,cjson)
 rm -Rf feeds/luci/{applications,collections,protocols,themes,libs}
 rm -Rf feeds/luci/modules/!(luci-base)
 
